6dd37307ce5d55ebe00e2382298c0983ee606b5c
EthanAmato <ethannamato@gmail.com>
2023-02-13 14:06:34 -0500

    completed 2/3 sections

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import { useState } from 'react'
 import './style.scss'
-import Img1 from './assets/images/image-web-3-desktop.jpg'
+import Spotlight from './assets/images/image-web-3-desktop.jpg'
+import Img1 from './assets/images/image-retro-pcs.jpg'
+import Img2 from './assets/images/image-top-laptops.jpg'
+import Img3 from './assets/images/image-gaming-growth.jpg'
 
 
 function App() {
@@ -21,36 +24,41 @@ function App() {
         </nav>
       </header>
       <main>
-        <section className='web-3'>
-          {/* <img src={Img1} alt='Web 3 video game console' /> */}
-          The Bright Future of Web 3.0?
-
-          We dive into the next evolution of the web that claims to put the power of the platforms back into the hands of the people.
-          But is it really fulfilling its promise?
-
-          Read more
+        <section className='spotlight'>
+          <img src={Spotlight} alt='Web 3 video game console' />
+          <div className='text'>
+            <h2>The Bright Future of Web 3.0?</h2>
+            <p>
+              We dive into the next evolution of the web that claims to put the power of the platforms back into the hands of the people.
+              But is it really fulfilling its promise?
+              <a href='#'>
+                Read more
+              </a>
+            </p>
+          </div>
         </section>
         <section className='new-articles'>
-
-          New
-
-          Hydrogen VS Electric Cars
-          Will hydrogen-fueled cars ever catch up to EVs?
-
-          The Downsides of AI Artistry
-          What are the possible adverse effects of on-demand AI image generation?
-
-          Is VC Funding Drying Up?
-          Private funding by VC firms is down 50% YOY. We take a look at what that means.
+          <h2>New</h2>
+          <a href='#'>Hydrogen VS Electric Cars</a>
+          <p>Will hydrogen-fueled cars ever catch up to EVs?</p>
+          <hr />
+          <a href='#'>The Downsides of AI Artistry</a>
+          <p>What are the possible adverse effects of on-demand AI image generation?</p>
+          <hr />
+          <a href='#'>Is VC Funding Drying Up?</a>
+          <p>Private funding by VC firms is down 50% YOY. We take a look at what that means.</p>
         </section>
-        <section className='top-3'>
+        <section className='recommend'>
+          <img src={Img1} />
           01
           Reviving Retro PCs
           What happens when old PCs are given modern upgrades?
+          <img src={Img2} />
 
           02
           Top 10 Laptops of 2022
           Our best picks for various needs and budgets.
+          <img src={Img3} />
 
           03
           The Growth of Gaming
